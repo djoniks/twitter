f7c46c1048ce043da77cd30f0620449b425118d9
Djonik <djon9d@mail.ru>
2014-09-17 14:38:43 +0000

    Set page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(title)
+    base_title = "Twitter"
+    if title.empty?
+      retun base_title
+    end
+     "#{base_title} | #{title}"
+  end
+   
 end
