63f8aab36ca59fcb2cce280b7dccaa4b9d8599d5
Djonik <djon9d@mail.ru>
2014-09-24 14:34:06 +0000

    Add user registration

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def full_title(title)
     base_title = "Twitter"
     if title.empty?
-      retun base_title
+      return base_title
     end
      "#{base_title} | #{title}"
   end
