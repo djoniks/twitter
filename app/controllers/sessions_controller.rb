bba85cbe98b52eca87c66083509e659e70531756
Djonik <djon9d@mail.ru>
2014-09-26 15:43:46 +0000

    Finish

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       flash[:success] = "Добро пожаловать!"
       sign_in user
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:error] = 'Попробуйте еще раз' # Not quite right!
       render 'new'
